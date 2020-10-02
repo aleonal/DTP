6486252bb6378d33eada988582e24c1a114be971
franciscon119 <72001808+franciscon119@users.noreply.github.com>
2020-10-02 01:27:22 -0600

    Update Sender - Nolasco Copy.py

--- Sender - Nolasco Copy.py
+++ Sender - Nolasco Copy.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 import _thread
 import time
 import string
@@ -30,27 +31,76 @@ def generate_payload(length=10):
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str 
 
+# Reads file in working directory as bytes
+def read_file(filename):
+    try:
+        with open(filename, 'rb') as f:
+            return f.read()
+    except OSError as e:
+        raise FileNotFoundError(e)
+
+# Given a payload, it returns the payload as a stack of packets ready to send using a
+# framed mechanism
+def package_payload(payload):
+    # We split up the payload into packets with packet size, while keeping track of the
+    # sequence number and the data to turn into a packet
+    packets = []
+    packet_data = b''
+    seq_num = 0
+
+    for i in range(len(payload)):
+        packet_data = packet_data + payload[i:i+1]
+        if len(packet_data) == PACKET_SIZE:
+            packets.append(packet.make(seq_num, packet_data))
+            packet_data = b''
+            seq_num += 1
+    if len(packet_data) > 0:
+        packets.append(packet.make(seq_num, packet_data))
+    
+    return packets
+
 # Send using Stop_n_wait protocol
 def send_snw(sock):
     # Fill out the code here
     global base
-    global total_packets
-    data = []
-    seq = 0
-    while (seq < 20):
-        data.append(generate_payload(40).encode())
-        seq = seq + 1
-    seq = 0
-    while (base < total_packets):
-        with mutex:
-            print ("base is",base)
-        pkt = packet.make(base, file_data[base])
-        print("Sending seq# ", base, "\n")
-        udt.send(pkt, sock, RECEIVER_ADDR)
-        time.sleep(TIMEOUT_INTERVAL)
-    pkt = packet.make(base, "END".encode())
-    while (base < 21):
-        udt.send(pkt, sock, RECEIVER_ADDR)
+    global timer
+    global mutex
+    try:
+        try:
+            payload = read_file(filename)
+        except FileNotFoundError:
+            print("File to send was not found. Data transfer terminated.")
+            return
+
+        packets = package_payload(payload)
+        nextseqnum = 0
+
+        _thread.start_new_thread(receive_snw, (sock, packets[len(packets)-1],))
+
+        while base < len(packets)-1:
+            with mutex:
+                if timer.timeout():
+                    timer.stop()
+                    print("Packet timed out. Resending packet")
+                    udt.send(packets[base], sock, RECEIVER_ADDR)
+                    print("Sent packet: {}\n".format(base))
+                    timer.start()
+                    #nextseqnum = base
+                    
+                elif not timer.running():
+                    udt.send(packets[base], sock, RECEIVER_ADDR)
+                    print("Sent packet: {}\n".format(base))
+                    timer.start()
+                    #nextseqnum += 1                    
+                #else:
+                #    print("packet is "+base)
+        print("File sent succesfully")
+        while base != -1:
+            udt.send(packet.make(-1, b'FIN'), sock, RECEIVER_ADDR)
+            time.sleep(TIMEOUT_INTERVAL)
+    except ConnectionResetError as e:
+        mutex.release()
+        print(e)
 
 # Send using GBN protocol
 def send_gbn(sock):
@@ -60,74 +110,38 @@ def send_gbn(sock):
 def receive_snw(sock, pkt):
     # Fill here to handle acks
     global base
-    global total_packets
-    while (base < total_packets+1):
-        print("starting udt recv\n")
-        pkt, receiveraddr = udt.recv(sock)
-        print("finished udt recv\n")
-        seq, data = packet.extract(pkt)
-        ackStr = data.decode()
-        print("From: ", receiveraddr, ", Seq# ", seq, ackStr)
-        with mutex:
-            base = seq + 1
-    return
+    global timer
+    global mutex
+    ackedpackets = 0
+    try:
+        fseqnum, fpayload = packet.extract(pkt)
+        while ackedpackets < fseqnum+1:
+            ack, addr = udt.recv(sock)
+            ackedpackets+=1
+
+            with mutex:
+                
+                seqnum, payload = packet.extract(ack)
+                base = seqnum + 1
+                
+                timer.stop()
+
+    except ConnectionError as e:
+        #mutex.release()
+        print(e)
 
 # Receive thread fro GBN
 def receive_gbn(sock):
     # Fill here to handle acks
     return
 
-
-
-# Define a function for the thread
-def print_time( threadName, delay):
-   count = 0
-   while count < 5:
-      time.sleep(delay)
-      count += 1
-      print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
-
 # Main function
 if __name__== '__main__':
-        # if len(sys.argv) != 2:
-    #     print('Expected filename as command line argument')
-    #     exit()
-    file = open("CS5313\\Homework 2\\bio.txt", "rb")
-
-    byte = file.read(1)
-    tempbytes = byte
-    while byte:
-        #print(tempbytes)
-        byte = file.read(1)
-        tempbytes = tempbytes + byte
-        if len(tempbytes) == 512:
-            file_data.append(tempbytes)
-            total_packets = total_packets + 1
-            print(tempbytes)
-            tempbytes = b''
-    if len(tempbytes) > 0:
-        file_data.append(tempbytes)
-        total_packets = total_packets + 1
-        print(tempbytes)
-
-    print("total packets = ",total_packets)
-
+    if len(sys.argv) != 2:
+        print('Expected filename as command line argument')
+        exit()    
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(SENDER_ADDR)
-    # filename = sys.argv[1]
-   
-
-    # Create two threads as follows
-    try:
-        _thread.start_new_thread(send_snw, (sock, ))
-    except:
-        print ("Error: unable to start thread 1")
-
-    try:
-        _thread.start_new_thread(receive_snw, (sock, packet.make(0, "".encode()), ))
-    except:
-        print ("Error: unable to start thread 2")
-    while (base < total_packets+1):
-        pass
-    
+    filename = sys.argv[1]
+    send_snw(sock)
     sock.close()
